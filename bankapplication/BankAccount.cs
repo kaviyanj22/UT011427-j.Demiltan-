6563b1320a51c231eb29bddc8700e0e6614e9b48
Joyjanas Demiltan <UT011427@unicomtic.com>
2026-05-20 16:57:16 +0530

    banking application

---
+++ bankapplication/BankAccount.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace bankapplication
+{
+    public class BankAccount
+    {
+
+        public string BankName;
+        public string AccHolderName;
+        public int AccNumber;
+        public double AccBalance;
+       
+    
+
+       
+        public BankAccount(string bankName, string accHolderName, int accNumber, double accBalance)
+        {
+            BankName = bankName;
+            AccHolderName = accHolderName;
+            AccNumber = accNumber;
+            AccBalance = accBalance;
+        }
+
+        // Display Metho
+        public void DisplayAccount()
+        {
+            Console.WriteLine("Bank Name : " + BankName);
+            Console.WriteLine("Account Holder Name : " + AccHolderName);
+            Console.WriteLine("Account Number : " + AccNumber);
+            Console.WriteLine("Account Balance : " + AccBalance);
+        }
+    }
+}
---
+++ bankapplication/Program.cs
@@ -0,0 +1,125 @@
+﻿using System;
+
+namespace bankapplication
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            BankAccount account = new BankAccount(
+                "BOE",
+                "Kaviyan",
+                1986460,
+                5000.00
+            );
+
+            BankName();
+            Welcome();
+            Menu(account);
+            bool exit = true;
+            while (exit)
+            {
+
+                Console.Write("Choose an option: ");
+
+                int option = Convert.ToInt32(Console.ReadLine());
+
+                switch (option)
+                {
+                    case 1:
+                        AccountDetails(account);
+
+
+                        break;
+
+                    case 2:
+                        CheckBalance(account);
+                        break;
+
+                    case 3:
+                        Deposit(account);
+                        break;
+
+                    case 4:
+                        Withdraw(account);
+                        break;
+
+                    case 5:
+                        exit = true;
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid Option");
+                        break;
+                }
+            }
+        }
+
+        static void BankName()
+        {
+            Console.WriteLine("=========================");
+            Console.WriteLine("-----------BOE-----------");
+            Console.WriteLine("=========================");
+        }
+
+        static void Welcome()
+        {
+            Console.WriteLine("WELCOME TO BANK OF EELAM");
+            Console.WriteLine("=========================");
+        }
+
+        static void Menu(BankAccount account)
+        {
+            Console.WriteLine("1.View Account Details");
+            Console.WriteLine("2.Check Balance");
+            Console.WriteLine("3.Deposit Money");
+            Console.WriteLine("4.Withdraw Money");
+            Console.WriteLine("5.Exit");
+            
+
+
+        }
+
+        static void AccountDetails(BankAccount account)
+        {
+            Console.WriteLine("\nAccount Details");
+            Console.WriteLine("Name: " + account.AccHolderName);
+            Console.WriteLine("Account Number: " + account.AccNumber);
+            Console.WriteLine("Balance: " + account.AccBalance);
+
+        }
+
+        static void CheckBalance(BankAccount account)
+        {
+            Console.WriteLine("\nCurrent Balance: " + account.AccBalance);
+        }
+
+        static void Deposit(BankAccount account)
+        {
+            Console.Write("\nEnter deposit amount: ");
+            double amount = Convert.ToDouble(Console.ReadLine());
+
+            if (amount > 0)
+            {
+                account.AccBalance += amount;
+                Console.WriteLine("Deposit Successful");
+            }
+        }
+
+        static void Withdraw(BankAccount account)
+        {
+            Console.Write("\nEnter withdrawal amount: ");
+            double amount = Convert.ToDouble(Console.ReadLine());
+
+            if (amount > 0 && amount <= account.AccBalance)
+            {
+                account.AccBalance -= amount;
+                Console.WriteLine("Withdraw Successful");
+            }
+            else
+            {
+                Console.WriteLine("Insufficient Balance");
+            }
+        }
+    }
+}
